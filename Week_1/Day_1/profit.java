98023eac924b077ed9cde2181da324c2b8b37c49
Lakshya <lakshyasharma2612@gmail.com>
2026-06-01 23:51:06 +0530

    first

---
+++ Week_1/Day_1/profit.java
@@ -0,0 +1,14 @@
+public class profit {
+public int maxProfit(int[] prices) {
+        int b = prices[0];
+        int p = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < b) {
+                b = prices[i];
+            } else if (prices[i] - b > p) {
+                p = prices[i] - b;
+            }
+        }
+        return p;
+    }
+}
---
+++ Week_1/Day_1/twosum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class twosum {
+public int[] twoSum(int[] nums, int target) {
+        
+       
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp)) {
+                return new int[]{map.get(comp), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{}; 
+}
+}
